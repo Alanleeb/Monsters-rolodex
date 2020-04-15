7c7f73f6b6a02e007f352d47dcad0cdaebf197ae
Alan Bischoff <alan_bischoff@Alans-MacBook-Pro-2019.local>
2020-04-15 17:00:46 -0600

    Updates

---
+++ precache-manifest.32fc3fa8de1768b2af1e194048aa597b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f81333423d6bd8334094647bbd4e622",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3da8fdba254f121b75f7",
+    "url": "/Monsters-rolodex/static/css/main.269da8c4.chunk.css"
+  },
+  {
+    "revision": "3c9d29464f6976dfc6d0",
+    "url": "/Monsters-rolodex/static/js/2.eeae8fd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-rolodex/static/js/2.eeae8fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da8fdba254f121b75f7",
+    "url": "/Monsters-rolodex/static/js/main.bce2a0b6.chunk.js"
+  },
+  {
+    "revision": "3ea7ef6908c1c2f541cb",
+    "url": "/Monsters-rolodex/static/js/runtime-main.66a8809a.js"
+  }
+]);
